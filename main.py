e68065d3afc33c74ece42415841456db28119d0b
jure <info@internet.com.tr>
2022-10-04 08:46:16 +0000

    files deleted

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import string.auth.username_generator_from_fullname
+foo = 1
+
+def func():
+    bar = 2
+    print(globals().keys()) # prints all variable names in global scope
+    print(locals().keys()) # prints all variable names in local scope
+
+func()
