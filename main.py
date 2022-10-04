c04aee2dfaf7d539f8c7f485b3dadc186768a87a
jure <info@internet.com.tr>
2022-10-04 08:49:07 +0000

    files deleted

--- main.py
+++
@@ -1,8 +0,0 @@
-foo = 1
-
-def func():
-    bar = 2
-    print(globals().keys()) # prints all variable names in global scope
-    print(locals().keys()) # prints all variable names in local scope
-
-func()
